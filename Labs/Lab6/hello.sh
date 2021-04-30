19520b987ca1bc988a34ac94f4cb58d11db73818
BobMingChang <83437600+BobMingChang@users.noreply.github.com>
2021-04-29 22:24:14 -0400

    Add files via upload

---
+++ Labs/Lab6/hello.sh
@@ -0,0 +1,17 @@
+#!/bin.bash
+echo "Welcome to computer science society."
+#Jonathan Cho
+#jcho18@student.gsu.edu
+now=$(date)
+echo "$now"
+echo "Current date: $now"
+echo */ | wc
+echo $PATH
+echo $USER
+echo $SHELL
+df
+echo "Please, could you loan me "$"25.00?"
+echo "if x=2, x*x=4, and x/2=1"
+echo *.sh|grep c
+echo "GoodBye!"
+date "+%H" 
